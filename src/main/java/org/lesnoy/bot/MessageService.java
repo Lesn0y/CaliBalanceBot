d036575b0436b43c7d66b8d5732ccd274e46c98e
Lesn0y <kilriljob@gmail.com>
2023-09-10 15:12:41 +0300

    Add new method for presentation products

--- src/main/java/org/lesnoy/bot/MessageService.java
+++ src/main/java/org/lesnoy/bot/MessageService.java
@@ -1,6 +1,9 @@
 package org.lesnoy.bot;
 
+import org.lesnoy.dto.ProductDTO;
 import org.lesnoy.dto.UserDTO;
+
+import java.util.List;
 
 public class MessageService {
 
@@ -19,4 +22,26 @@ public class MessageService {
         return builder.toString();
     }
 
+    public String getProductsInfo(List<ProductDTO> products) {
+        StringBuilder builder = new StringBuilder();
+
+        for (ProductDTO element : products) {
+            builder.append(element.getName());
+            builder.append(" - ");
+            builder.append(element.getProductType().getName());
+            builder.append(" - ");
+            builder.append(element.getCal());
+            builder.append("/");
+            builder.append(element.getProt());
+            builder.append("/");
+            builder.append(element.getFats());
+            builder.append("/");
+            builder.append(element.getCarbs());
+            builder.append("\n");
+        }
+
+        return builder.toString();
+
+
+    }
 }
