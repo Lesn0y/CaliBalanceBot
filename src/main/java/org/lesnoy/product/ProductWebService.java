df87b3a469f818a2bda281694ff18a9b87dce172
Lesn0y <kilriljob@gmail.com>
2023-09-23 12:23:46 +0300

    Add new method

--- src/main/java/org/lesnoy/product/ProductWebService.java
+++ src/main/java/org/lesnoy/product/ProductWebService.java
@@ -1,10 +1,9 @@
 package org.lesnoy.product;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import okhttp3.*;
 import org.lesnoy.exeptions.WebApiExeption;
 
 import java.io.IOException;
@@ -14,6 +13,63 @@ import java.util.List;
 public class ProductWebService {
 
     private final String serverUrl = "http://localhost:8080";
+
+    public Product saveProduct(Product product) throws WebApiExeption {
+        OkHttpClient httpClient = new OkHttpClient();
+
+        ObjectMapper mapper = new ObjectMapper();
+        String json = null;
+        try {
+            json = mapper.writeValueAsString(product);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        RequestBody body = RequestBody.create(json, MediaType.parse("application/json"));
+
+        Request request = new Request.Builder()
+                .url(serverUrl + "/api/v1/products")
+                .post(body)
+                .build();
+
+        try (Response response = httpClient.newCall(request).execute()) {
+            if (response.code() == 201) {
+                String jsonResponse = response.body().string();
+                return mapper.readValue(jsonResponse, Product.class);
+            }
+            throw new WebApiExeption("Произошла ошибка при сохранении продукта " + product.getName());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    public List<Product> findProductByName(String productName) throws WebApiExeption {
+        OkHttpClient httpClient = new OkHttpClient();
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        Request request = new Request.Builder()
+                .url(serverUrl + "/api/v1/products?name=" + productName)
+                .build();
+
+        try (Response response = httpClient.newCall(request).execute()) {
+            if (response.code() == 200) {
+                String jsonResponse = response.body().string();
+                JsonNode jsonArray = mapper.readTree(jsonResponse);
+
+                List<Product> products = new ArrayList<>();
+                for (JsonNode element : jsonArray) {
+                    products.add(mapper.treeToValue(element, Product.class));
+                }
+                return products;
+            }
+            throw new WebApiExeption("Продукт " + productName + " не найден");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
 
     public List<Product> findAllProductsByOwnerAndType(String login, int typeId) throws WebApiExeption {
         OkHttpClient httpClient = new OkHttpClient();
