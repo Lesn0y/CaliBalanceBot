d0a8787ffdd4af5e7d1fc9dbedcc848faa394b66
Lesn0y <kilriljob@gmail.com>
2023-11-13 20:37:57 +0300

    Separate session parameters

---
+++ src/main/java/org/lesnoy/bot/MenuButton.java
@@ -0,0 +1,27 @@
+package org.lesnoy.bot;
+
+import java.util.Arrays;
+
+public enum MenuButton {
+    START("/start"),
+    PRODUCT_MENU("Меню продуктов"),
+    ADD_DISHES("Добавить прием пищи"),
+    CALORIES_LEFT("Остаток КБЖУ"),
+    CALORIES_DAILY("Суточное КБЖУ");
+
+    private final String massage;
+
+    MenuButton(String massage) {
+        this.massage = massage;
+    }
+    public String getMassage() {
+        return massage;
+    }
+
+    public static MenuButton parseString(String value) throws Exception {
+        return Arrays.stream(MenuButton.values())
+                .filter(btn -> btn.getMassage().equals(value))
+                .findFirst().orElseThrow(Exception::new);
+
+    }
+}
