db9553ae8c04192c55758d9f19c4f2aea6606d66
Lesn0y <kilriljob@gmail.com>
2023-11-13 20:37:11 +0300

    reworked the operation of the keyboard provider

--- src/main/java/org/lesnoy/bot/KeyboardProvider.java
+++ src/main/java/org/lesnoy/bot/KeyboardProvider.java
@@ -12,9 +12,12 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.Keyboard
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ResourceBundle;
 
 public class KeyboardProvider {
-    
+
+    private static final String buttons = "buttons";
+
     public static ReplyKeyboardMarkup getDefaultKeyboard() {
         ReplyKeyboardMarkup keyboard = new ReplyKeyboardMarkup();
 
@@ -23,19 +26,19 @@ public class KeyboardProvider {
         
         ArrayList<KeyboardRow> keyboardRows = new ArrayList<>();
         KeyboardRow keyboardRow1 = new KeyboardRow();
-        keyboardRow1.add("Остаток КБЖУ");
+        keyboardRow1.add(ResourceBundle.getBundle(buttons).getString("main_menu_1"));
         keyboardRows.add(keyboardRow1);
 
         KeyboardRow keyboardRow2 = new KeyboardRow();
-        keyboardRow2.add("Меню продуктов");
+        keyboardRow2.add(ResourceBundle.getBundle(buttons).getString("main_menu_2"));
         keyboardRows.add(keyboardRow2);
 
         KeyboardRow keyboardRow3 = new KeyboardRow();
-        keyboardRow3.add("Добавить прием пищи");
+        keyboardRow3.add(ResourceBundle.getBundle(buttons).getString("main_menu_3"));
         keyboardRows.add(keyboardRow3);
 
         KeyboardRow keyboardRow4 = new KeyboardRow();
-        keyboardRow4.add("Суточное КБЖУ");
+        keyboardRow4.add(ResourceBundle.getBundle(buttons).getString("main_menu_4"));
         keyboardRows.add(keyboardRow4);
 
         keyboard.setKeyboard(keyboardRows);
@@ -51,14 +54,14 @@ public class KeyboardProvider {
         List<InlineKeyboardButton> row3 = new ArrayList<>();
         List<InlineKeyboardButton> row4 = new ArrayList<>();
 
-        InlineKeyboardButton button1 = new InlineKeyboardButton("Добавить новый продукт");
+        InlineKeyboardButton button1 = new InlineKeyboardButton(ResourceBundle.getBundle(buttons).getString("product_menu_1"));
         button1.setCallbackData(ProductOption.ADD_PRODUCT.name());
-        InlineKeyboardButton button2 = new InlineKeyboardButton("Посмотреть список своих продуктов");
+        InlineKeyboardButton button2 = new InlineKeyboardButton(ResourceBundle.getBundle(buttons).getString("product_menu_2"));
         button2.setCallbackData(ProductOption.OWN_PRODUCTS.name());
-        InlineKeyboardButton button3 = new InlineKeyboardButton("Просмотреть список всех продуктов");
+        InlineKeyboardButton button3 = new InlineKeyboardButton(ResourceBundle.getBundle(buttons).getString("product_menu_3"));
         button3.setCallbackData(ProductOption.ALL_PRODUCTS.name());
-        InlineKeyboardButton button4 = new InlineKeyboardButton("Назад");
-        button4.setCallbackData("exit");
+        InlineKeyboardButton button4 = new InlineKeyboardButton(ResourceBundle.getBundle(buttons).getString("menu_back"));
+        button4.setCallbackData(ProductOption.EXIT.name());
 
         row1.add(button1);
         row2.add(button2);
@@ -124,8 +127,8 @@ public class KeyboardProvider {
 
         List<InlineKeyboardButton> row = new ArrayList<>();
         InlineKeyboardButton button = new InlineKeyboardButton();
-        button.setText("Назад");
-        button.setCallbackData("menu");
+        button.setText(ResourceBundle.getBundle(buttons).getString("menu_back"));
+        button.setCallbackData(String.valueOf(-1));
         row.add(button);
         rows.add(row);
 
--- src/main/java/org/lesnoy/bot/SessionAttribute.java
+++
@@ -1,7 +0,0 @@
-package org.lesnoy.bot;
-
-public enum SessionAttribute {
-    NEW_USER,
-    PRODUCT,
-    ADD_ENTRY
-}
